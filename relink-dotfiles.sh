7ada8191e67e0d8d2a94326eb029d3fa004ca1d5
Tomas Rejhons <t.rejhons@gmail.com>
2021-06-02 17:20:17 +0200

    add link script

---
+++ relink-dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# tmux
+rm ~/.tmux.conf
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+#zsh
+rm ~/.zshrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+
+# vim
+rm ~/.vimrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
