b7cdad7186955ee5841a88fa3a49d703a13b258d
Tomas Rejhons <t.rejhons@gmail.com>
2022-02-01 20:43:51 +0100

    update

--- nvim/lua/tomas/telescope.lua
+++ nvim/lua/tomas/telescope.lua
@@ -28,6 +28,7 @@ require("telescope").load_extension("git_worktree")
 require('telescope').load_extension('fzy_native')
 
 local M = {}
+
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
         prompt_title = "< VimRC >",
