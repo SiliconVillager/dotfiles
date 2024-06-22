132ba12e17015c683b1090cc976d03770cef3b30
Tomas Rejhons <t.rejhons@gmail.com>
2024-06-22 10:41:48 +0200

    Update

--- nvim/after/plugin/gdscript.lua
+++ nvim/after/plugin/gdscript.lua
@@ -1,5 +1,4 @@
-local port = os.getenv('GDScript_Port') or '6005'
-local cmd = vim.lsp.rpc.connect('127.0.0.1', port)
+local cmd = vim.lsp.rpc.connect('127.0.0.1', 6005)
 local pipe = '/tmp/godot.pipe'
 
 vim.lsp.start({
--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -57,4 +57,17 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+require 'lspconfig'.gdscript.setup {
+  on_attach = function (client)
+    local _notify = client.notify
+    client.notify = function (method, params)
+      if method == 'textDocument/didClose' then
+          -- Godot doesn't implement didClose yet
+          return
+      end
+      _notify(method, params)
+    end
+  end
+}
+
 lsp.setup()
