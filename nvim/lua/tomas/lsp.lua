d4e51cb56804229854b255c0ca05ec168f53d479
Tomas Rejhons <t.rejhons@gmail.com>
2026-01-31 12:42:40 +0100

    Update

--- nvim/lua/tomas/lsp.lua
+++ nvim/lua/tomas/lsp.lua
@@ -1,61 +1,64 @@
-local lsp = require('lsp-zero').preset({})
-
-lsp.preset("recommended")
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+local cmp_mappings = {
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	['<CR>']  = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
-
-        cmp_mappings['<Tab>'] = nil
-        cmp_mappings['<S-Tab>'] = nil
-
-        lsp.setup_nvim_cmp({
-              mapping = cmp_mappings
-          })
-
+	['<Up>']   = cmp.mapping.select_prev_item(cmp_select),
+  ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
+}
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+cmp.setup({
+  mapping = cmp_mappings,
+  sources = {
+    { name = 'nvim_lsp' },
+  }
 })
 
-lsp.on_attach(function(client, bufnr)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local function on_attach(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp.default_keymaps({buffer = bufnr})
 
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+end
 
 -- (Optional) Configure lua language server for neovim
-vim.lsp.config('lua_ls', lsp.nvim_lua_ls())
+vim.lsp.config('lua_ls', {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = { globals = { 'vim' } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+    },
+  },
+})
 
-vim.lsp.config('gdscript', {})
+vim.lsp.config('gdscript', {
+  capabilities = capabilities
+})
 
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('gdscript')
+
