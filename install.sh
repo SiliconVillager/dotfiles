d421e7149977fa6e5970be496b73ecb122b1688d
Tomas Rejhons <t.rejhons@gmail.com>
2026-02-02 16:44:25 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Install fonts
+FONTS_DIR="$HOME/.local/share/fonts"
+if [ -d "$FONTS_DIR" ] || [ -L "$FONTS_DIR" ]; then
+		rm -rf "$FONTS_DIR"
+fi
+ln -s "$(pwd)/fonts" "$FONTS_DIR"
+
 # Process config files
 cd ./config
 ./install.sh
 cd ..
+
